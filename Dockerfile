21850c1044b3ceda7c7ccf60e603725445684bac
xiaoz <xiaoz93@outlook.com>
2022-07-06 09:49:42 +0800

    add

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM redis:6-alpine3.16
+
+WORKDIR /root
+#复制启动文件
+COPY *.sh /root/
+# 复制zdir配置文件
+COPY *.conf /root/
+RUN sh install.sh
+# 复制php.ini
+COPY ./php.ini /etc/php7/
+VOLUME /data/wwwroot/default/data
+EXPOSE 80
+CMD ["/usr/sbin/run.sh"]
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+##########		name:Onenav容器运行入口			##########
+##########		update:2021/03/22				##########
+##########		author:xiaoz<xiaoz93@outlook.com>	######
+
+onenav_path='/data/wwwroot/default'
+
+#start redis
+redis-server
+#start php-fpm
+php-fpm7 -RD
+#start nginx
+/usr/sbin/nginx
+tail -f /var/log/nginx/error.log
